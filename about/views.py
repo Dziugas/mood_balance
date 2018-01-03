ed70a1665d89d86cf6a8773f37adc977f55a1ea1
Dziugas <dziugas@graphity.org>
2018-01-03 19:38:17 +0200

    view function names changed according to functions

--- about/views.py
+++ about/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
+def about(request):
     return HttpResponse("<h1>Hola! This is me!</h1>")
--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
+def home(request):
     return HttpResponse("<h1>Welcome to our homepage!</h1><h2>Linkai:</h2>")
