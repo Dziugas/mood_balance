efe916b9ba039f29a9adec31b3661fcb66be2b64
Dziugas <dziugas@graphity.org>
2018-01-09 21:07:08 +0200

    fixed  uris

--- contact/views.py
+++ contact/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def contact(request):
-    return render(request, 'contact/jumbotronHeader.html')
+    return render(request, 'contact/contact.html')
