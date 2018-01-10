c0fb89860adbd1bb76f85c87be8779c71fef151c
Dziugas <dziugas@graphity.org>
2018-01-10 15:30:04 +0200

    migrated the blog model to the db

--- blog/models.py
+++ blog/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Article(models.Model):
     title = models.CharField(max_length=100)
-    slug = models.slugField()
-    body = models.textField()
+    slug = models.SlugField()
+    body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
 
 #add in later:
