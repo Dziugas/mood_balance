c63dea175fd17753f3ad8a79620306cb64098533
Dziugas <dziugas@graphity.org>
2018-01-11 13:01:01 +0200

    Set up the static in urls.py and settings.py, linked styles.css to blog app

--- mood_balance/urls.py
+++ mood_balance/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -8,3 +10,5 @@ urlpatterns = [
     url((r'^contact/'), include('contact.urls')),
     url((r'^blog/'), include('blog.url'))
 ]
+
+urlpatterns += staticfiles_urlpatterns()
