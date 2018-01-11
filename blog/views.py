c7c60905eda39a7f723899c792cff17f09acfe91
Dziugas <dziugas@graphity.org>
2018-01-11 12:11:31 +0200

    Added html template tags -> published the articles

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Article
 
 def blog(request):
-    return render (request, 'blog/blog.html')
+    articles = Article.objects.all().order_by('date')
+    return render (request, 'blog/blog.html', {'articles': articles})
