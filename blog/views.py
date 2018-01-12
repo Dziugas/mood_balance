258cc58ba729659e4eeb2ee36b3a3f5042cb2e6d
Dziugas <dziugas@graphity.org>
2018-01-12 17:47:58 +0200

    Created and linked article_details

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from .models import Article
 from django.http import HttpResponse
 
+
 def blog(request):
     articles = Article.objects.all().order_by('date')
     return render (request, 'blog/blog.html', {'articles': articles})
 
 def article_detail(request, slug):
-    return HttpResponse(slug)
+    article = Article.objects.get(slug = slug)
+    return render(request, 'blog/article_detail.html', {'article': article})
