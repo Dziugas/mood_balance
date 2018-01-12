140dfc67f14e35aa0584eb6b4c58ec7e9b385f47
Dziugas <dziugas@graphity.org>
2018-01-12 17:09:33 +0200

    Created links from blog list to article details

--- blog/url.py
+++ blog/url.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
-    url(r'^', views.blog, name='blog')
+    url(r'^$', views.blog, name='blog'),
+    url(r'^(?P<slug>[\w-]+)/$', views.article_detail, name='detail'),
 ]
 
 
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .models import Article
+from django.http import HttpResponse
 
 def blog(request):
     articles = Article.objects.all().order_by('date')
     return render (request, 'blog/blog.html', {'articles': articles})
+
+def article_detail(request, slug):
+    return HttpResponse(slug)
