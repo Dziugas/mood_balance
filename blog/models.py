8cce1f45fcce63904a15259960c05c91a22bfbd0
Dziugas <dziugas@graphity.org>
2018-01-14 13:34:14 +0200

    Added media for the blogposts

--- blog/models.py
+++ blog/models.py
@@ -5,8 +5,10 @@ class Article(models.Model):
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+    thumb = models.ImageField(default='default.png', blank=True)
+
+
 #add in later:
-#    thumbnail
 #    author
 
     def __str__(self):
